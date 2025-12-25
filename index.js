37e332fdb98ffdf63ca0389a85b9cfd863f37279
Devashree-999 <devashreemaity@gmail.com>
2025-12-25 10:30:23 +0530

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require("express")
-const{connection}=require("./config/db")
+require("dotenv").config()
+const connectDB=require("./config/db")
 const {userRouter}=require("./routes/user.routes")
 const router=require("./routes/mess.routes")
 const cors=require("cors")
@@ -19,15 +20,17 @@ app.get("/",(req,res)=>{
     res.send({msg:"home page"})
 })
 
-app.listen(8080,async()=>{
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,async()=>{
     try {
-        await connection
+        await connectDB()
         console.log("Database connected");
         
     } catch (error) {
         console.log("unable to connect data",{error:error.message});
         
     }
-    console.log("server is listening");
+    console.log(`server is listening on port ${PORT}`);
     
 })
